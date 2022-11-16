10c147ec4fb3a7e7d0d14b646f9ef8fac950c5d2
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2022-11-16 19:58:15 +0800

    removed config for firebase

--- backend/src/configs/firebase.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import admin from 'firebase-admin';
-const serviceAccount = Buffer.from(process.env.SERVICE_ACCOUNT, 'base64');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount.toString('utf-8')),
-});
-
-export default admin;
